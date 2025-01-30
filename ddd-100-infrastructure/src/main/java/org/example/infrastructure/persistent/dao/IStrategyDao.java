fc93ec2513cdc4057df29cc2bbf54ce9e8307c23
Tan Yawen <2749934612@qq.com>
2025-01-30 15:18:12 +0800

    feat:第4节 基础层持久化数据

---
+++ ddd-100-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IStrategyDao.java
@@ -0,0 +1,18 @@
+package org.example.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.infrastructure.persistent.po.Strategy;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 抽奖策略 DAO
+ * @create 2023-12-16 13:24
+ */
+@Mapper
+public interface IStrategyDao {
+
+    List<Strategy> queryStrategyList();
+
+}
