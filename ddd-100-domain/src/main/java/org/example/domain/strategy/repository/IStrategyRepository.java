43ad38616418ee5c038e43fddfc02b2ae50d5081
Tan Yawen <2749934612@qq.com>
2025-01-31 13:35:19 +0800

    feat:第6节 策略权重概率装配

--- ddd-100-domain/src/main/java/org/example/domain/strategy/repository/IStrategyRepository.java
+++ ddd-100-domain/src/main/java/org/example/domain/strategy/repository/IStrategyRepository.java
@@ -1,7 +1,10 @@
 package org.example.domain.strategy.repository;
 
 
+
 import org.example.domain.strategy.model.entity.StrategyAwardEntity;
+import org.example.domain.strategy.model.entity.StrategyEntity;
+import org.example.domain.strategy.model.entity.StrategyRuleEntity;
 
 import java.util.List;
 import java.util.Map;
@@ -15,10 +18,16 @@ public interface IStrategyRepository {
 
     List<StrategyAwardEntity> queryStrategyAwardList(Long strategyId);
 
-    void storeStrategyAwardSearchRateTable(Long strategyId, Integer rateRange, Map<Integer, Integer> strategyAwardSearchRateTable);
+    void storeStrategyAwardSearchRateTable(String key, Integer rateRange, Map<Integer, Integer> strategyAwardSearchRateTable);
 
-    Integer getStrategyAwardAssemble(Long strategyId, Integer rateKey);
+    Integer getStrategyAwardAssemble(String key, Integer rateKey);
 
     int getRateRange(Long strategyId);
+
+    int getRateRange(String key);
+
+    StrategyEntity queryStrategyEntityByStrategyId(Long strategyId);
+
+    StrategyRuleEntity queryStrategyRule(Long strategyId, String ruleModel);
 
 }
