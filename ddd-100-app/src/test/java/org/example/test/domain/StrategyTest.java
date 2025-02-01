b6aead107b0bb282692e5db26264ad310eb5c2cf
Tan Yawen <2749934612@qq.com>
2025-02-01 20:04:01 +0800

    feat:第11节 模板模式串联抽奖规则

--- ddd-100-app/src/test/java/org/example/test/domain/StrategyTest.java
+++ ddd-100-app/src/test/java/org/example/test/domain/StrategyTest.java
@@ -1,9 +1,10 @@
 package org.example.test.domain;
 
-import lombok.extern.slf4j.Slf4j;
 import org.example.domain.strategy.service.armory.IStrategyArmory;
 import org.example.domain.strategy.service.armory.IStrategyDispatch;
 import org.example.infrastructure.persistent.redis.IRedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.api.RMap;
@@ -28,27 +29,34 @@ public class StrategyTest {
 
     @Resource
     private IStrategyDispatch strategyDispatch;
-    @Test
+
+    /**
+     * 策略ID；100001L、100002L 装配的时候创建策略表写入到 Redis Map 中
+     */
+    @Before
     public void test_strategyArmory() {
-        boolean success = strategyArmory.assembleLotteryStrategy(100001L);
+        boolean success = strategyArmory.assembleLotteryStrategy(100005L);
         log.info("测试结果：{}", success);
     }
-
-
 
     /**
      * 从装配的策略中随机获取奖品ID值
      */
     @Test
-    public void test_getAssembleRandomVal() {
-        log.info("测试结果：{} - 奖品ID值", strategyDispatch.getRandomAwardId(100001L));
+    public void test_getRandomAwardId() {
+         log.info("测试结果：{} - 奖品ID值", strategyDispatch.getRandomAwardId(100001L));
     }
+
+    /**
+     * 根据策略ID+权重值，从装配的策略中随机获取奖品ID值
+     */
     @Test
     public void test_getRandomAwardId_ruleWeightValue() {
         log.info("测试结果：{} - 4000 策略配置", strategyDispatch.getRandomAwardId(100001L, "4000:102,103,104,105"));
         log.info("测试结果：{} - 5000 策略配置", strategyDispatch.getRandomAwardId(100001L, "5000:102,103,104,105,106,107"));
         log.info("测试结果：{} - 6000 策略配置", strategyDispatch.getRandomAwardId(100001L, "6000:102,103,104,105,106,107,108,109"));
     }
+
     @Resource
     private IRedisService redisService;
 
@@ -95,15 +103,6 @@ public class StrategyTest {
         for (Map.Entry<Integer, Integer> entry : randomizedMap.entrySet()) {
             System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
-    }
-
-    @Test
-    public void test_getAssembleRandomval(){
-        log.info("测试结果{} - 奖品ID值",strategyDispatch.getRandomAwardId(100002L));
-        log.info("测试结果{} - 奖品ID值",strategyDispatch.getRandomAwardId(100002L));
-        log.info("测试结果{} - 奖品ID值",strategyDispatch.getRandomAwardId(100002L));
-
-
     }
 
 }
